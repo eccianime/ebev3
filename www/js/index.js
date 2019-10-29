9944573d5500869e61a9d0e4505cf0c1d7a6fa34
unknown <eccianime@gmail.com>
2019-10-29 03:04:59 -0400

    Prueba de red

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,28 @@ function PGcargado(){
 		$(".splash").fadeOut(3000);
 	}, 3000);*/
 
+	setInterval( function () {
+		checkConnection();
+	}, 1000 );
+
 }
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    $("#con").html(states[networkState]);
+}
+
 
 function CORS ( url, respuesta ) {
 	/*var loading = "<div class='splash'></div>";*/
